e47ae7dde0a6f5c73f4f47ea86bf835cc7d87f53
Victor <victorac2324@gmail.com>
2024-03-18 13:47:43 -0400

    used scope to manipulate functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+   return customerName = customerName.toUpperCase()
+   }
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+// // overwrites best customer name to a new variable
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+  
+  return bestCustomer
+}
+
+const leastFavoriteCustomer = "Victor"
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Rachel"
+ return leastFavoriteCustomer
+}
+
+// console.log (overwriteBestCustomer)
